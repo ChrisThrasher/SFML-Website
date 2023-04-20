668fdfed77ec30084e611a4f9095aa7635bf33a8
Chris Thrasher <chrisjthrasher@gmail.com>
2023-04-20 12:42:51 -0600

    Add tutorial page for CMake template

---
+++ tutorials/2.6/start-cmake.php
@@ -0,0 +1,31 @@
+<?php
+
+    $title = "SFML with CMake Project Template";
+    $page = "start-cmake.php";
+
+    require("header.php");
+
+?>
+
+<h1>SFML with CMake Project Template</h1>
+
+<?php h2('Introduction') ?>
+<p>
+    This tutorial will work on any OS, with any IDE, and with any compiler.
+    It will explain how to build a project that can be used with any release, branch, or Git commit of SFML.
+    This approach is unique in that it eliminates the possibility of linker errors and makes it as easy as possible to upgrade your SFML version in the future.
+</p>
+
+<h3>Getting Started</h3>
+<p>
+    https://github.com/SFML/cmake-sfml-project
+</p>
+<p>  
+    Read the repository's README for instructions on getting started.
+</p>
+
+<?php
+
+    require("footer.php");
+
+?>
